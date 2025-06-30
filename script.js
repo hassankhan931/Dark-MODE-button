904cce3bc914254472d1f2ec1a8d4c0678ee5481
Hassan Khan <hassankhan2091k@gmail.com>
2025-06-30 09:14:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let btn = document.querySelector("button");
+let Darkmode = false;
+let btn = document.querySelector(".btn");
+btn.addEventListener("click",()=>{
+    Darkmode = !Darkmode;
+    if (Darkmode){
+        document.body.style.backgroundColor = "black" ;
+    } else {
+        document.body.style.backgroundColor = "white" ;
+    }
+});
+// by Hassan Khan.
